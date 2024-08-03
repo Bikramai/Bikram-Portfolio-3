71a2066127be025a8fe3b2d4e2deb1432ca2208c
bikramia <bikram.rhce@gmail.com>
2024-08-03 13:27:01 -0400

    added link repository - Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center">
-      <a href="" target="_blank" className="text-center  md:text-[14px] text-sm  font-semibold py-4">
+      <a href="https://github.com/Bikramai/Bikram-Portfolio-3" target="_blank" className="text-center  md:text-[14px] text-sm  font-semibold py-4">
         Created by Bikram - View Repository
       </a>
     </div>
