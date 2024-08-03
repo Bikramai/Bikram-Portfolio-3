1c25b227a949edcf258060f7efee14ad189fd2df
bikramia <bikram.rhce@gmail.com>
2024-08-03 09:39:48 -0400

    Footer - Changed text fonts size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center">
-      <a href="" target="_blank" className="text-center  md:text-[16px] text-sm  font-semibold py-4">
+      <a href="" target="_blank" className="text-center  md:text-[14px] text-sm  font-semibold py-4">
         Created by Bikram - View Repository
       </a>
     </div>
