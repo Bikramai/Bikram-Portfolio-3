5dbcd9881692cb8569a8f90665ac080d298a62ca
bikramia <bikram.rhce@gmail.com>
2024-08-03 15:36:38 -0400

    Changed the link to hover -Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ const Footer = () => {
   return (
     <div className="text-center">
       <a href="https://github.com/Bikramai/Bikram-Portfolio-3" target="_blank" className="text-center  md:text-[14px] text-sm  font-semibold py-4 ">
-        Created by Bikram - View Repository
+        Created by Bikram - 
+        <a href="" target="_blank" className="cursor-pointer hover:text-[#00CEA8] ">
+        View Repository
+        </a>
       </a>
     </div>
   );
